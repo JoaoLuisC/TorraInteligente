162cb0bc3a8da7aa9be3336285d11589f557be79
João Luís Cardoso <joao.lc12@hotmail.com>
2025-07-17 21:09:24 -0300

    aaaaa

--- database/migrations/2025_07_12_223604_create_torradors_table.php
+++ database/migrations/2025_07_12_223604_create_torradors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('torradors', function (Blueprint $table) {
+        Schema::create('torradores', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->string('codigo_conexao');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('torradors');
+        Schema::dropIfExists('torradores');
     }
 };
--- database/migrations/2025_07_17_234358_create_torradors_table.php
+++ database/migrations/2025_07_17_234358_create_torradors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('torradors', function (Blueprint $table) {
+        Schema::create('torradores', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('usuario_id');
             $table->string('nome', 100);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('torradors');
+        Schema::dropIfExists('torradores');
     }
 };
